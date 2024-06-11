b236d57dbab368f66f4fc747ce160346115f55b4
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-06-11 13:23:41 +0100

    Make divs appear as an n * n grid

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const body = document.querySelector("body");
-const heading = document.createElement("h1");
-const container = document.createElement("div");
-const btn = document.createElement("button");
-
-heading.textContent = "Etch-a-Sketch";
-btn.textContent = "Choose grid dimension";
-
-body.appendChild(heading);
-body.appendChild(btn);
-body.appendChild(container);
+const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
+  
   const dimension = +prompt("Enter your preferred grid dimension:");
   for (let i = 0; i < dimension; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
+    for (let j = 0; j < dimension; j++) {
+      const innerDiv = document.createElement("div");
+      innerDiv.setAttribute("class", "innerDiv");
+      div.appendChild(innerDiv);
+    }
   }
 });
