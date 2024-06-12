2edb7af4e5e86d2a02d02819149848457c063c2d
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-06-12 12:41:06 +0100

    Clear grid on each button click

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const container = document.querySelector(".container");
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
-  
+  container.innerHTML = ""; // Clears grid on each button click
   const dimension = +prompt("Enter your preferred grid dimension:");
+
   for (let i = 0; i < dimension; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
+
     for (let j = 0; j < dimension; j++) {
       const innerDiv = document.createElement("div");
       innerDiv.setAttribute("class", "innerDiv");
