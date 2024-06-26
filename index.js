ba06fa54c277898dbb0274ea5ec7c05327dca991
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-06-26 09:46:18 +0100

    Add mouseover event for inner divs

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-import setHexColor from "./utils/colors";
+import setHexColor from "./utils/colors.js";
 
 const container = document.querySelector(".container");
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
   container.innerHTML = ""; // Clears grid on each button click
-  const dimension = +prompt("Enter your preferred grid dimension:");
+  const dimension = +prompt(
+    "Enter your preferred grid dimension\n(Should not exceed 100):"
+  );
+  if (dimension <= 100) {
+    for (let i = 0; i < dimension; i++) {
+      const div = document.createElement("div");
+      container.appendChild(div);
 
-  for (let i = 0; i < dimension; i++) {
-    const div = document.createElement("div");
-    container.appendChild(div);
+      for (let j = 0; j < dimension; j++) {
+        const innerDiv = document.createElement("div");
+        innerDiv.setAttribute("class", "innerDiv");
+        div.appendChild(innerDiv);
 
-    for (let j = 0; j < dimension; j++) {
-      const innerDiv = document.createElement("div");
-      innerDiv.setAttribute("class", "innerDiv");
-      div.appendChild(innerDiv);
+        innerDiv.addEventListener("mouseover", (event) => {
+          event.target.style.backgroundColor = setHexColor();
+        });
+      }
     }
+  } else {
+    alert(
+      "DANGER!!! Dimension cannot exceed 100!\nClick on 'Choose grid dimension' to play again."
+    );
   }
 });
